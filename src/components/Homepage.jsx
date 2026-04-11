7d9977443d163f8a39ec0102587bfa16bef4685b
Seth <sdarmistead@gmail.com>
2026-04-12 01:16:03 +0200

    made the mars page fancy

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,11 +8,11 @@ const Homepage = () => {
         <div className="z-10">Welcome to your very own Satellyte!</div>
         <img src={`${getMediaURL("satellite.jpg")}`} className="w-full object-cover opacity-30 grayscale-100 group-hover:opacity-85 group-hover:grayscale-0 duration-200 absolute" />
       </div>
-      <Link to="/mars" state={{title: "Mars"}} className="gridItem box row-span-6 group relative overflow-hidden">
+      <Link to="/mars" state={{title: "Mars"}} className="gridItem hover:scale-[101%] box row-span-6 group relative overflow-hidden">
         <video className="h-full object-cover opacity-30 grayscale-100 group-hover:opacity-85 group-hover:grayscale-0 duration-200 absolute" src={`${getMediaURL("mars.mp4")}`} loop autoPlay muted></video>
         <div className="z-10">Check the <span className="group-hover:font-bold duration-100">&nbsp;Martian&nbsp;</span>weather...</div>
       </Link>
-      <Link to="/asteroids" state={{title: "Asteroids"}} className="gridItem box relative row-span-4 overflow-hidden group">
+      <Link to="/asteroids" state={{title: "Asteroids"}} className="gridItem hover:scale-[101%] box relative row-span-4 overflow-hidden group">
         <div className="z-10">Search for <span className=" group-hover:font-bold duration-100">&nbsp;asteroids&nbsp;</span> approaching the earth..</div>
         <img src={`${getMediaURL("asteroid.jpg")}`} className="min-w-full min-h-full object-cover opacity-30 grayscale-100 group-hover:opacity-85 group-hover:grayscale-0 duration-200 absolute" />
       </Link>
--- src/pages/Mars.jsx
+++ src/pages/Mars.jsx
@@ -11,15 +11,19 @@ const Mars = ({photoDisplay, setPhotoDisplay}) => {
       {photoDisplay && <Display photoDisplay={photoDisplay} setPhotoDisplay={setPhotoDisplay} defaultTitle={"Mars"}/>}
       <div className="w-full flex flex-col items-center justify-center">
         <div className="flex relative">
-          <video className="w-250 h-fit shadow-[0_0_50px_40px] rounded-[500px] shadow-[#040202]" src={`${getMediaUrl("mars.mp4")}`} loop autoPlay muted></video>
-          {data && <div className="absolute px-42 py-6 flex flex-col justify-between h-full w-full text-white text-lg font-semibold">
+          <div className="aspect-7/3 pt-8 pb-50 bg-radial from-[#923b22] to-black/80 rounded-[500px] shadow-[8px_12px_40px_20px] shadow-black">
+            <div className="ml-[30%] bg-black w-fit aspect-square flex overflow-hidden justify-center rounded-full">
+              <video className="h-[60vh] scale-130 aspect-square object-cover shadow-[0_0_50px_40px] shadow-[#21120d]" src={`${getMediaUrl("mars.mp4")}`} loop autoPlay muted></video>
+            </div>
+          </div>
+          {data && <div className="absolute px-42 py-18 flex flex-col justify-between h-full w-full text-white text-lg font-semibold">
             <div >
-              <div>Avg Atmospheric Pressure: {data && data[data.sol_keys[0]].PRE.av}</div>
-              <div className="text-transform: capitalize">Season: {data && data[data.sol_keys[0]].Season}</div>
+              <div className="mars">Avg Atmospheric Pressure: {data && data[data.sol_keys[0]].PRE.av}</div>
+              <div className="mars text-transform: capitalize">Season: {data && data[data.sol_keys[0]].Season}</div>
             </div>
             <div className="flex justify-between">
-              <div>Avg Atmospheric Temp: {data && data[data.sol_keys[0]].AT.av}</div>
-              <div>Avg Horizontal Wind Speed: {data && data[data.sol_keys[0]].HWS.av}</div>
+              <div className="mars">Avg Atmospheric Temp: {data && data[data.sol_keys[0]].AT.av}</div>
+              <div className="mars">Avg Horizontal Wind Speed: {data && data[data.sol_keys[0]].HWS.av}</div>
             </div>
           </div>}
         </div>
